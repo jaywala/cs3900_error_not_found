69d6b2f482dcddf2dc09329db60ecd7599ad4c48
Gladys Chan <glad_c12@hotmail.com>
2018-08-20 15:49:43 +1000

    basic advertisement model done, need to add amenities

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 class Advertisement(models.Model):
+
     accommodation_name = models.CharField(max_length=50)
+    accommodation_description = models.CharField(max_length=1000, default='')
+
+    house_rules = models.CharField(max_length=1000, default='')
+    booking_rules = models.CharField(max_length=1000, default='')
+
     base_price = models.IntegerField(default=0)
+
+    num_guests = models.IntegerField(default=0)
+    num_bedrooms = models.IntegerField(default=0)
+    num_bathrooms = models.IntegerField(default=0)
+
+    suburb = models.CharField(max_length=100, default='')
+    state = models.CharField(default='NSW', max_length=50)
+    country = models.CharField(default='Australia', max_length=50)
 
     def __str__(self):
         return self.accommodation_name
