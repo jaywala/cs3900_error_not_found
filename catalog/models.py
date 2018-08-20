7d9683f8d95527072764a01aebc82a85ea5344dc
Gladys Chan <glad_c12@hotmail.com>
2018-08-20 14:41:46 +1000

    made admin/superuser page

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Advertisment(models.Model):
+    accommodation_name = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.accommodation_name
