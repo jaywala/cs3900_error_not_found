b11380da85b0a518a83ffbc5743e2fcbfff49ee7
GeorgeQ-Q <593635548@qq.com>
2018-08-27 20:28:27 +1000

    george first deliverable

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -62,13 +62,22 @@
       </header>
     <router-view></router-view>
     <br>
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
   </div>
 </template>
 <script>
 import AuthService from './auth/AuthService'
 import axios from 'axios'
-import HelloWorld from './components/HelloWorld.vue'
 import newbook from '@/components/newbook.vue'
+import landing from '@/components/landing.vue'
 const API_URL = 'http://localhost:8000'
 const auth = new AuthService()
 export default {
@@ -85,7 +94,6 @@ export default {
       message: ''
     }
   },
-  components: {HelloWorld},
   methods: {
     // this method calls the AuthService login() method
     login () {
