fb3bcdbadd6a432fbc78abfdd4fdbe4f2c6b1e8f
Gladys Chan <glad_c12@hotmail.com>
2018-08-27 14:40:43 +1000

    fixed calendar bug

--- catalog/models.py
+++ catalog/models.py
@@ -12,6 +12,21 @@ class User_Profile(models.Model):
 
     def __str__(self):
         return self.user_name
+'''
+    def get_user_name(self):
+        return self.user_name
+
+    def get_name(self):
+        return self.name
+
+    def get_email(self):
+        return self.email
+
+    def set_name(primary_key, new_name):
+        u = Catalog.objects.get(id=primary_key)
+        u.name = name
+        u.save(['name'])
+'''
 
 
 class User_Review(models.Model):
@@ -77,44 +92,57 @@ class PropertyImage(models.Model):
 class Event(models.Model):
 
     advert = models.ForeignKey(Advertisement, related_name='events', on_delete=models.CASCADE)
-    day = models.DateField(u'Day of the event', help_text=u'Day of the event')
-    start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
-    end_time = models.TimeField(u'Final time', help_text=u'Final time')
+
+    start_day = models.DateField(u'Start day of the rent', help_text=u'Start day of the rent')
+    start_day_start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
+
+    end_day = models.DateField(u'End day of the event', help_text=u'End day of the event')
+    end_day_end_time = models.TimeField(u'Final time', help_text=u'Final time')
+
+    booking_status = 'booked'
+
     notes = models.TextField(u'Textual Notes', help_text=u'Textual Notes', blank=True, null=True)
 
     class Meta:
         verbose_name = u'Scheduling'
         verbose_name_plural = u'Scheduling'
 
-    def check_overlap(self, fixed_start, fixed_end, fixed_day, new_start, new_end, new_day):
-        # TODO: 
-        """
-        TODO need to check if the same day and also if it was a previous fixed event but due to
-        a previous error it's checking itself again. need to somehow differenciate events
-        that was previously booked.
-        """
+    def check_overlap(self, fixed_start_day, fixed_start_day_start_time, fixed_end_day, fixed_end_day_end_time,
+                      new_start_day, new_start_day_start_time, new_end_day, new_end_day_end_time):
+
         overlap = False
-        if new_start == fixed_end or new_end == fixed_start:    #edge case
-            overlap = False
-        elif (new_start >= fixed_start and new_start <= fixed_end) or (new_end >= fixed_start and new_end <= fixed_end): #innner limits
+        if fixed_start_day == new_end_day or fixed_end_day == new_start_day:
+            if fixed_start_day_start_time <= new_end_day_end_time or fixed_end_day_end_time >= new_end_day_end_time:
+                overlap = True
+        elif fixed_start_day == new_start_day or fixed_end_day == new_end_day:
             overlap = True
-        elif new_start <= fixed_start and new_end >= fixed_end: #outter limits
+        elif (fixed_start_day >= new_start_day and fixed_end_day <= new_end_day) or (fixed_start_day <= new_start_day and fixed_end_day >= new_end_day):
+            overlap = True
+        elif (fixed_start_day >= new_start_day and fixed_end_day <= new_start_day) or (fixed_start_day <= new_start_day and fixed_end_day >= new_start_day):
             overlap = True
 
         return overlap
 
     def get_absolute_url(self):
         url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
-        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
+        return u'<a href="%s">%s</a>' % (url, str(self.start_day))
 
     def clean(self):
-        if self.end_time <= self.start_time:
-            raise ValidationError('Ending times must after starting times')
+        if self.start_day > self.end_day:
+            raise ValidationError('Ending day, ' + str(self.end_day) + ', must be after starting day ' + str(self.start_day))
+        if self.start_day == self.end_day and self.start_day_start_time >= self.end_day_end_time:
+            raise ValidationError('Ending time, ' + str(self.end_day_end_time) + ', must be after starting time ' + str(self.start_day_start_time
+            ) + ', since it\'s on the same day')
 
-        events = Event.objects.filter(day=self.day)
+        events = Event.objects.all()
         if events.exists():
             for event in events:
-                if self.check_overlap(event.start_time, event.end_time, event.day, self.start_time, self.end_time, self.day):
+                if self.check_overlap(event.start_day, event.start_day_start_time, event.end_day, event.end_day_end_time,
+                                      self.start_day, self.start_day_start_time, self.end_day, self.end_day_end_time
+                                      ) and event.id != self.id:
                     raise ValidationError(
-                        'There is an overlap with another event: ' + str(event.day) + ', ' + str(
-                            event.start_time) + '-' + str(event.end_time))
+                         'There is an overlap with another event: ' + 'the new event --> self ' + str(self.notes
+                         ) + ' ' + str(self.booking_status) + ', ' + 'old event --> event ' + str(event.notes
+                         ) + ' ' + str(event.booking_status))
+                        #'There is an overlap with another event: ' + str(event.start_day) + ', ' + str(
+                        #    event.start_day_start_time) + '-' + str(event.end_day) + ', ' + str(event.end_day_end_time))
