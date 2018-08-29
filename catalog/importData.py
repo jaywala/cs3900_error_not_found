748c43204eada6760c6e2bd4b5e01b783c1dee5d
Jay Motwani <jymotwani@hotmail.com>
2018-08-29 10:59:16 +1000

    Read from CSV File

---
+++ catalog/importData.py
@@ -0,0 +1,10 @@
+
+    # function importFromCSV():
+    #     with open('user.csv') as csvfile:
+    #         next(csvfile)
+    #         readcsv = csv.reader(csvfile, delimiter=',')
+    #         for row in readcsv:
+    #             if row[4]  == "trainee":
+    #                 self._users.append(trainee(row[0],row[1],row[2],row[3]))
+    #             elif row[4] == "trainer":
+    #                 self._users.append(trainer(row[0],row[1],row[2],row[3]))
