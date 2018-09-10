11119f355b538511909ea9217c7cc9ebeb7e76a2
Gladys Chan <glad_c12@hotmail.com>
2018-09-10 17:13:34 +1000

    fixed a bug in the event check_overlap function

--- catalog/models.py
+++ catalog/models.py
@@ -335,18 +335,19 @@ class Event(models.Model):
         verbose_name_plural = u'Scheduling'
 
     def check_overlap(self, fixed_start_day, fixed_start_day_start_time, fixed_end_day, fixed_end_day_end_time,
-                      new_start_day, new_start_day_start_time, new_end_day, new_end_day_end_time):
+                      new_start_day, new_start_day_start_time, new_end_day, new_end_day_end_time, event):
 
         overlap = False
-        if fixed_start_day == new_end_day or fixed_end_day == new_start_day:
-            if fixed_start_day_start_time <= new_end_day_end_time or fixed_end_day_end_time >= new_end_day_end_time:
+        if self.advert.pk == event.advert.pk:
+            if fixed_start_day == new_end_day or fixed_end_day == new_start_day:
+                if fixed_start_day_start_time <= new_end_day_end_time or fixed_end_day_end_time >= new_end_day_end_time:
+                    overlap = True
+            elif fixed_start_day == new_start_day or fixed_end_day == new_end_day:
                 overlap = True
-        elif fixed_start_day == new_start_day or fixed_end_day == new_end_day:
-            overlap = True
-        elif (fixed_start_day >= new_start_day and fixed_end_day <= new_end_day) or (fixed_start_day <= new_start_day and fixed_end_day >= new_end_day):
-            overlap = True
-        elif (fixed_start_day >= new_start_day and fixed_end_day <= new_start_day) or (fixed_start_day <= new_start_day and fixed_end_day >= new_start_day):
-            overlap = True
+            elif (fixed_start_day >= new_start_day and fixed_end_day <= new_end_day) or (fixed_start_day <= new_start_day and fixed_end_day >= new_end_day):
+                overlap = True
+            elif (fixed_start_day >= new_start_day and fixed_end_day <= new_start_day) or (fixed_start_day <= new_start_day and fixed_end_day >= new_start_day):
+                overlap = True
 
         return overlap
 
@@ -366,7 +367,7 @@ class Event(models.Model):
             for event in events:
                 if self.check_overlap(event.start_day, event.start_day_start_time, event.end_day, event.end_day_end_time,
                                       self.start_day, self.start_day_start_time, self.end_day, self.end_day_end_time
-                                      ) and event.id != self.id:
+                                      , event) and event.id != self.id:
                     raise ValidationError(
                          'There is an overlap with another event: ' + 'the new event --> self ' + str(self.notes
                          ) + ' ' + str(self.booking_status) + ', ' + 'old event --> event ' + str(event.notes
