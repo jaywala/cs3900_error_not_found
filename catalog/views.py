41cc8c8693229f5ffc3a0dd5fb4e306d6539b839
Gladys Chan <glad_c12@hotmail.com>
2018-09-13 00:40:20 +1000

    tried getting the api working. added APIService.js in auth directory and edited detail.vue

--- catalog/views.py
+++ catalog/views.py
@@ -12,6 +12,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Advertisement model
 @api_view(['GET', 'POST'])
@@ -20,9 +22,28 @@ def advertisement_list(request):
     List all Advertisements, or create a new Advertisement.
     """
     if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
         ad = Advertisement.objects.all()
-        serializer = AdvertisementSerializer(ad, many=True)
-        return Response(serializer.data)
+        page = request.GET.get('page', 1)
+        paginator = Paginator(advertisement, 10)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = AdvertisementSerializer(ad, context={'request: request'}, many=True)
+        if data.has_next():
+            nextPage = data.next_page_number()
+        if data.has_previous:
+            previous_page = data.previous_page_number()
+
+        return Response({'data': serializer.data , 'count': paginator.count,
+        'numpages' : paginator.num_pages, 'nextlink': '/advertisement/?page=' + str(nextPage),
+        'prevlink': '/advertisement/?page=' + str(previousPage)})
 
     elif request.method == 'POST':
         serializer = AdvertisementSerializer(data=request.data)
@@ -31,7 +52,7 @@ def advertisement_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def advertisement_detail(request, pk):
     """
     Retrieve, update or delete an advertisement.
@@ -42,11 +63,11 @@ def advertisement_detail(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = AdvertisementSerializer(ad)
+        serializer = AdvertisementSerializer(ad, context={'request: request'})
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = AdvertisementSerializer(data=request.data)
+    elif request.method == 'PUT':
+        serializer = AdvertisementSerializer(ad, data=request.data, context={'request: request'})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
--- frontend/src/components/detail.vue
+++ frontend/src/components/detail.vue
@@ -1,39 +1,59 @@
 <template >
   <div id="app">
-  <h1>{{this.errored}}</h1>
+  <h1>{{this.ad}}</h1>
 
 </div>
 </template>
 
 <script>
 import axios from 'axios'
+import {APIService} from '../auth/APIService';
+/*import Loading from './Loading';*/
+const API_URL = 'http://localhost:8000';
+const apiService = new APIService();
+
 export default {
-  data () {
-    return {
-        /*token:*/
-      info: {'username': 'admin', 'password': 'admin'},
-      my_data:
-      {
-          "user": 1,
-          "accommodation_name": "changed",
-          "accommodation_description": "Very central",
-          "house_rules": "Be considerate.   No showering after 2330h.",
-          "booking_rules": "no running",
-          "base_price": 65.0,
-          "num_guests": 1,
-          "num_bedrooms": 1,
-          "num_bathrooms": 0,
-          "suburb": "NSW",
-          "state": "NSW",
-          "country": "Australia",
-          "latitude": -33.86916827,
-          "longitude": 151.2265622
+    data () {
+        return {
+            "user": 1,
+            "accommodation_name": "An Oasis in the City",
+            "accommodation_description": "Very central ",
+            "house_rules": "Be considerate.   No showering after 2330h.",
+            "booking_rules": "no running",
+            "base_price": 65.0,
+            "num_guests": 1,
+            "num_bedrooms": 1,
+            "num_bathrooms": 0,
+            "suburb": "Potts Point",
+            "state": "NSW",
+            "country": "Australia",
+            "latitude": -33.86916827,
+            "longitude": 151.2265622
+        };
     },
-    errored: false
-
+    methods: {
+        getAdvertisements(){
+            apiService.getAdvertisement().then((page) => {
+            });
+        },
+        mounted() {
+            axios
+              .get('http://localhost:8000/advertisement/1/')
+              .then(response => {
+                this.info = response.data
+              })
+              .catch(error => {
+                console.log(error)
+                this.info.errored = 'get method has error'
+              })
+              .finally(() => this.loading = false)
+        },
     }
-  },
+}
 
+
+
+/*
   mounted () {
     axios
       .get('http://localhost:8000/advertisement/1/')
@@ -45,13 +65,13 @@ export default {
         this.info.errored = 'get method has error'
       })
       .finally(() => this.loading = false)
-    /*
+
     axios
         .post('http://localhost:8000/advertisement/1/', this.info)
         .catch(error => {
             console.log(error)
             this.info.errored = 'post method has error'
-        })*/
+        })
     axios
         .post('http://localhost:8000/login/', this.info)
         .catch(error => {
@@ -67,7 +87,7 @@ export default {
 
 
   }
-}
+}*/
 </script>
 
 <style lang="css">
