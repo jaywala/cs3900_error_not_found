73a11704ed339cf0d9c5eabb6a0b2faef9c25482
Joseph Hilsberg <jqhils@Josephs-MacBook-Pro-2.local>
2018-10-02 15:56:19 +1000

    Basic details template.

--- frontend/src/components/landing.vue
+++ frontend/src/components/landing.vue
@@ -20,10 +20,10 @@
             <!-- Each Listing -->
             <div v-for="n in 10" class="col-md-4">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51MZEBXRYML._SL500_AC_SS350_.jpg" alt="Card image cap">
+                  <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51MZEBXRYML._SL500_AC_SS350_.jpg" alt="Card image cap">
                 <div class="card-body">
                   <p class="card-text">&lt;Property Type&gt; · &lt;Location&gt;</p>
-                  <h4 class="card-text">&lt;Listing Title&gt;</h4>
+                  <a href="/detail/1"><h4 class="card-text">&lt;Listing Title&gt;</h4></a>
                   <p class="card-text">$&lt;Cost&gt; AUD per night</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <small class="text-muted">&lt;Rating&gt; &lt;# of Reviews&gt;</small>
