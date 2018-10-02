9c53aa7fc2ccf2b2c342b27dc2e24928d080cce0
Joseph Hilsberg <jqhils@Josephs-MacBook-Pro-2.local>
2018-10-02 14:14:38 +1000

    reverted app.vue changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,77 +1,76 @@
 <template>
-    <div id = "app">
-        <!-- Bootstrap core CSS -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+  <div id = "app">
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- Custom styles for this template -->
-        <link href="./components/album.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="./components/album.css" rel="stylesheet">
 
-        <header>
-          <div class="collapse bg-dark" id="navbarHeader">
-            <div class="container">
-              <div class="row">
-                <div class="col-sm-8 col-md-7 py-4">
-                  <h4 class="text-white">About</h4>
-                  <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-                </div>
-                <div class="col-sm-4 offset-md-1 py-4">
-                  <h4 class="text-white">Contact</h4>
-                  <ul class="list-unstyled">
-                    <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                    <li><a href="#" class="text-white">Like on Facebook</a></li>
-                    <li><a href="#" class="text-white">Email me</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div class="navbar navbar-dark bg-dark box-shadow">
-            <div class="container d-flex justify-content-between">
-              <a href="#" class="navbar-brand d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-                <strong>NotAirbnb</strong>
-              </a>
-
-              <form class="m-md-2 navbar-form navbar-left" role="search">
-                <div class="form-group">
-                  <input type="text" class="form-control" placeholder="Search">
-                </div>
-              </form>
-
-              <div class="ddown">
-                <b-dropdown id="ddown1" text="Dropdown Button" class="m-md-2">
-                  <b-dropdown-item class="btn btn-primary btn-margin" v-if="!authenticated" @click="login()">
-                    Log In
-                  </b-dropdown-item>
-                  <b-dropdown-item class="btn btn-primary btn-margin" v-if="authenticated" @click="logout()">
-                    Log Out
-                  </b-dropdown-item>
-
-                      <router-link to="/newbook" v-if="authenticated()" tag = "b-dropdown-item">add room</router-link>
-
-                      <router-link to="/editprofile" v-if="authenticated()" tag = "b-dropdown-item">edit profile</router-link>
-
-                  <b-dropdown-item>
-                    <router-link to="/newbook" v-if = "authenticated">add room</router-link>
-                  </b-dropdown-item>
-
-                </b-dropdown>
-              </div>
-            </div>
-          </div>
-        </header>
-      <router-view></router-view>
-      <br>
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
         <div class="container">
-          <p class="float-right">
-            <a href="#">Back to top</a>
-          </p>
-          <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-          <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">About</h4>
+              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Contact</h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Follow on Twitter</a></li>
+                <li><a href="#" class="text-white">Like on Facebook</a></li>
+                <li><a href="#" class="text-white">Email me</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </header>
+      </div>
+
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+            <strong>NotAirbnb</strong>
+          </a>
+
+          <form class="m-md-2 navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+          </form>
+
+          <div class="ddown">
+            <b-dropdown id="ddown1" text="Dropdown Button" class="m-md-2">
+              <b-dropdown-item class="btn btn-primary btn-margin" v-if="!authenticated" @click="login()">
+                Log In
+              </b-dropdown-item>
+              <b-dropdown-item class="btn btn-primary btn-margin" v-if="authenticated" @click="logout()">
+                Log Out
+              </b-dropdown-item>
+
+              <b-dropdown-divider></b-dropdown-divider>
+
+              <b-dropdown-item>
+                <router-link to="/newbook" v-if = "authenticated">add room</router-link>
+              </b-dropdown-item>
+
+            </b-dropdown>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <router-view></router-view>
     <br>
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
   </div>
 </template>
 
