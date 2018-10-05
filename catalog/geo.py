02ec4af27d38129fe37814431de413088840acb7
Jay Motwani <jymotwani@hotmail.com>
2018-10-05 17:33:40 +1000

    fix return

--- catalog/geo.py
+++ catalog/geo.py
@@ -3,5 +3,5 @@ from geopy.geocoders import Nominatim
 def Position(address):
     geolocator = Nominatim(user_agent="Google")
     location = geolocator.geocode("address")
-    return (location.latitude, location.longitude))
+    return (location.latitude, location.longitude)
 
