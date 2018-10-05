3f1c45ff5b0e71ea91badeced7e934fbf9531a1a
Jay Motwani <jymotwani@hotmail.com>
2018-10-05 17:33:02 +1000

    indentation fix

--- catalog/geo.py
+++ catalog/geo.py
@@ -2,6 +2,6 @@ from geopy.geocoders import Nominatim
 
 def Position(address):
     geolocator = Nominatim(user_agent="Google")
-       location = geolocator.geocode("address")
-       return (location.latitude, location.longitude))
+    location = geolocator.geocode("address")
+    return (location.latitude, location.longitude))
 
