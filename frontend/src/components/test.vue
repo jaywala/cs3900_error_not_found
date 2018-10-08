6631374da300a9198b2bc29d2ee580cec5e017f6
GeorgeQ-Q <593635548@qq.com>
2018-10-08 16:11:53 +1100

    george

--- frontend/src/components/test.vue
+++ frontend/src/components/test.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
     <div class="text-center">
-      <img src="" alt="" :src="img.pic" v-for="img in message">
+
     </div>
+
+    <Slider
+    :pagination-visible="true"
+    :pagination-clickable = "true"
+      :async-data="message"
+      direction="horizontal">
+    <div v-for="img in message" :key="img.id">
+        <img :src="img.pic" alt="">
+    </div>
+  </Slider>
+
+
   </div>
 </template>
 <script>
-
+//<img src="" alt="" :src="img.pic" v-for="img in message">
 import Vue from 'vue'
 import axios from 'axios'
 import router from '../router'
 import auth from '../auth'
-
+import Slider from 'vue-plain-slider'
 export default {
+  components: {
+      Slider
+   },
   methods: {
       // this method calls the AuthService login() method
       login () {
@@ -30,26 +45,37 @@ export default {
       },
       get(){
           return router.app.$auth.getUserProfile()
-      }
+      },
+      onSlideChangeStart (currentPage, el) {
+      console.log('onSlideChangeStart', currentPage, el);
+    },
+    onSlideChangeEnd (currentPage, el) {
+      console.log('onSlideChangeEnd', currentPage, el);
+    }
   },
 
   data() {
     return {
       message: null,
+      slickOptions: {
+                slidesToShow: 3,
+                // Any other options that can be got from plugin documentation
+      },
     }
   },
 
   mounted () {
-
       axios.get("http://localhost:8000/get/advertisement/images/"+ "Colleen/example/1/")
       .then(response => {
            // JSON responses are automatically parsed.
            this.message = response.data
+           console.log(this.message)
        })
        .catch(e => {
            this.errors.push(e)
        })
        console.log(this.message)
+
    }
 /*
     mounted () {
